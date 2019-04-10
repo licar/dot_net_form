12a8d545f73e58a48cd2ae42c6a649ae36c381a3
kim.ranim <evgeniy.polikarpov95@gmail.com>
2019-04-10 13:56:17 +0300

    Added services and stores

---
+++ TestServer/Services/FormService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TestServer.Core.Models;
+
+namespace TestServer.Services
+{
+    public class FormService : IFormService
+    {
+        public async Task Add()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<FormModel>> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ TestServer/Services/IFormService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TestServer.Core.Models;
+
+namespace TestServer.Services
+{
+    public interface IFormService
+    {
+        Task<IEnumerable<FormModel>> Get();
+        Task Add();
+    }
+}
